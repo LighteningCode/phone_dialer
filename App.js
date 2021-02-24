b43b2ece70a46509af37fcefafe9bcd1faeb2b35
Elvis Agbesi <elvisagbesi@gmail.com>
2021-02-24 00:16:29 +0000

    feat: :art: basic and functioning

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+let device_width = Dimensions.get("window").width;
+let device_height = Dimensions.get("window").height;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <View style={styles.dialer_view}>
+
+        <TouchableOpacity
+          onPress={() => console.log("Hello")}
+          style={styles.button}>
+          <Text style={{ color: "white", fontSize: 30 }}>1</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => console.log("Hello")}
+          style={styles.button}>
+          <Text style={{ color: "white", fontSize: 30 }}>1</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => console.log("Hello")}
+          style={styles.button}>
+          <Text style={{ color: "white", fontSize: 30 }}>1</Text>
+        </TouchableOpacity>
+
+      </View>
     </View>
   );
 }
@@ -18,4 +40,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#32a895',
+    fontWeight: 'bold',
+    padding: 10,
+    marginHorizontal: 10,
+    width: 70,
+    height: 70,
+    borderRadius: 1200,
+  },
+  dialer_view: {
+    flex: 1,
+    marginVertical: 40,
+    justifyContent: 'center',
+    flexDirection: 'row',
+  }
 });
