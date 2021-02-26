a8afde3fabcccf56e50b45c7f155d1f7c7db3caa
Elvis Agbesi <elvisagbesi@gmail.com>
2021-02-26 21:17:17 +0000

    feat: :construction: component change

--- App.js
+++ App.js
@@ -6,7 +6,17 @@ let device_width = Dimensions.get("window").width;
 let device_height = Dimensions.get("window").height;
 
 
-function PhoneButton({ children, input, _onPress }) {
+function PhoneButton({input, _onPress,number }) {
+  return (
+    <TouchableOpacity
+      onPress={() => (typeof _onPress === 'function') ? _onPress(input) : ""}
+      style={styles.button}>
+        <Text style={styles.dialer_text}>{number}</Text>
+    </TouchableOpacity>
+  )
+}
+
+function CustomPhoneButton({ children, input, _onPress }) {
   return (
     <TouchableOpacity
       onPress={() => (typeof _onPress === 'function') ? _onPress(input) : ""}
@@ -46,79 +56,55 @@ export default function App() {
     <View style={styles.container}>
       <StatusBar style="auto" />
 
-      <View style={{ marginTop: 50 }}>
+      <View style={{ marginTop: 50, backgroundColor:"blue",height:device_height }}>
 
-        <View style={{ marginTop: 50, paddingHorizontal: 20,marginBottom:50 }}>
+        <View style={{ marginTop: 50, paddingHorizontal: 20,paddingVertical: 50,marginBottom:10, backgroundColor:"red" }}>
           <Text style={{ fontSize: 50, textAlign: "center" }}>{dial}</Text>
         </View>
 
-        <View style={{ flexDirection: 'column', height: 400, paddingHorizontal: 150 }}>
+        <View style={{ flexDirection: 'column', height: 450, paddingHorizontal: 150, backgroundColor:"yellow" }}>
 
           <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center' }}>
-            <PhoneButton input={1} _onPress={handlePress}>
-              <Text style={{ color: "white", fontSize: 30 }}>1</Text>
-            </PhoneButton>
+            <PhoneButton input={1} _onPress={handlePress} number={1} />
 
-            <PhoneButton input={2} _onPress={handlePress}>
-              <Text style={{ color: "white", fontSize: 30 }}>2</Text>
-            </PhoneButton>
+            <PhoneButton input={2} _onPress={handlePress} number={2} />
 
-            <PhoneButton input={3} _onPress={handlePress}>
-              <Text style={{ color: "white", fontSize: 30 }}>3</Text>
-            </PhoneButton>
+            <PhoneButton input={3} _onPress={handlePress} number={3} />
           </View>
 
           <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center' }}>
-            <PhoneButton input={4} _onPress={handlePress}>
-              <Text style={{ color: "white", fontSize: 30 }}>4</Text>
-            </PhoneButton>
+            <PhoneButton input={4} _onPress={handlePress} number={4} />
 
-            <PhoneButton input={5} _onPress={handlePress}>
-              <Text style={{ color: "white", fontSize: 30 }}>5</Text>
-            </PhoneButton>
+            <PhoneButton input={5} _onPress={handlePress} number={5} />
 
-            <PhoneButton input={6} _onPress={handlePress}>
-              <Text style={{ color: "white", fontSize: 30 }}>6</Text>
-            </PhoneButton>
+            <PhoneButton input={6} _onPress={handlePress} number={6} />
           </View>
 
           <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center' }}>
-            <PhoneButton input={7} _onPress={handlePress}>
-              <Text style={{ color: "white", fontSize: 30 }}>7</Text>
-            </PhoneButton>
+            <PhoneButton input={7} _onPress={handlePress} number={7} />
 
-            <PhoneButton input={8} _onPress={handlePress}>
-              <Text style={{ color: "white", fontSize: 30 }}>8</Text>
-            </PhoneButton>
+            <PhoneButton input={8} _onPress={handlePress} number={8} />
 
-            <PhoneButton input={9} _onPress={handlePress}>
-              <Text style={{ color: "white", fontSize: 30 }}>9</Text>
-            </PhoneButton>
+            <PhoneButton input={9} _onPress={handlePress} number={9} />
           </View>
 
           <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center' }}>
-            <PhoneButton input={"*"} _onPress={handlePress}>
-              <Text style={{ color: "white", fontSize: 30 }}>*</Text>
-            </PhoneButton>
+            <PhoneButton input={"*"} _onPress={handlePress} number={"*"} />
 
-            <PhoneButton input={0} _onPress={handlePress}>
-              <Text style={{ color: "white", fontSize: 30 }}>0</Text>
-            </PhoneButton>
+            <PhoneButton input={0} _onPress={handlePress} number={0} />
 
-            <PhoneButton input={"#"} _onPress={handlePress}>
-              <Text style={{ color: "white", fontSize: 30 }}>#</Text>
-            </PhoneButton>
+            <PhoneButton input={"#"} _onPress={handlePress} number={"#"} />
           </View>
 
           <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center' }}>
 
-            <PhoneButton>
-              <Text style={{ color: "white", fontSize: 30 }}>Call</Text>
-            </PhoneButton>
+            <CustomPhoneButton>
+              <Text style={{ color: "black", fontSize: 30 }}>Call</Text>
+            </CustomPhoneButton>
 
-            <PhoneButton _onPress={handleDelete}>
-              <Text style={{ color: "white", fontSize: 30 }}>Del</Text>
-            </PhoneButton>
+            <CustomPhoneButton _onPress={handleDelete}>
+              <Text style={{ color: "black", fontSize: 30 }}>Del</Text>
+            </CustomPhoneButton>
           </View>
 
         </View>
@@ -133,17 +119,23 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    height: device_height
   },
   button: {
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#32a895',
+    backgroundColor: '#e8e8e8',
     fontWeight: 'bold',
-    padding: 10,
+    padding: 5,
     margin: 10,
-    width: 70,
-    height: 70,
+    width: 75,
+    height: 75,
     borderRadius: 1200,
+  },
+  dialer_text:{
+    color: "#2b2b2b",
+    fontSize: 30,
+    fontWeight: '600'
   },
   dialer_view: {
     flex: 1,
