52a51cf70d29366cc78bd2a839d20b6f4415a038
Elvis Agbesi <elvisagbesi@gmail.com>
2021-02-28 22:37:10 +0000

    feat: :construction: Created Recents.js file

--- screens/Recents.js
+++ screens/Recents.js
@@ -1,16 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useRef, useState } from 'react';
-import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Dimensions, SafeAreaView, StyleSheet, Text, TouchableOpacity, View, FlatList } from 'react-native';
+import { Ionicons, FontAwesome5 } from '@expo/vector-icons';
 
 let device_width = Dimensions.get("window").width;
 let device_height = Dimensions.get("window").height;
 
+
+function RecentListItem({ item }) {
+    return (
+        <View style={{ justifyContent: 'space-around', flexDirection: 'row', height: 50, paddingHorizontal: 20 }}>
+            <View style={{ flex: 1, alignSelf: 'center' }}>
+                <Ionicons name="call-sharp" size={15} />
+            </View>
+
+            <View style={{ flex: 5, alignSelf: 'center', alignContent: 'flex-start' }}>
+                <Text style={{ alignSelf: 'start', fontSize: 18, fontWeight: '700' }}>{item.key}</Text>
+                <Text style={{ alignSelf: 'start', color: "#bfbfbf" }}>mobile</Text>
+            </View>
+            <Text style={{ flex: 2, alignSelf: 'center', fontWeight: '400', textAlign: 'right' }}>{item.time}</Text>
+            <View style={{ flex: 1, alignSelf: 'center' }}>
+                <Ionicons style={{ alignSelf: 'flex-end' }} color="#08a4ff" name="information-circle-outline" size={20} />
+            </View>
+
+        </View>
+    )
+}
+
 function Recents() {
     return (
-        <View>
+        <SafeAreaView style={{ ...styles.container }}>
             <StatusBar style="auto" />
-            <Text>hello</Text>
-        </View>
+            <View style={{ width: device_width }}>
+
+                <View>
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 10, paddingVertical: 10, width: device_width, marginBottom: 10 }}>
+                        <View style={{ alignSelf: 'center', flex: 1, alignItems: 'flex-end' }}>
+                        </View>
+                        <View style={{ alignSelf: 'center', flex: 1 }}>
+                            
+                        </View>
+                        <Text style={{ fontSize: 20, alignSelf: 'flex-end', color: "#3385ff", textAlign:'right',flex: 1 }}>Edit</Text>
+                    </View>
+                </View>
+
+                <View style={{ paddingLeft: 20 }}>
+                    <Text style={{ fontSize: 40, fontWeight: '700' }}>Recents</Text>
+                </View>
+                <FlatList
+                    data={[
+                        { key: 'Devin', time: "12:59pm" },
+                        { key: 'Dan', time: "12:59pm" },
+                        { key: 'Dominic', time: "12:59pm" },
+                        { key: 'Jackson', time: "12:59pm" },
+                        { key: 'James', time: "12:59pm" },
+                    ]}
+                    renderItem={({ item }) => <RecentListItem item={item} />}
+                />
+
+            </View>
+
+
+        </SafeAreaView>
     )
 }
 
@@ -29,6 +80,9 @@ const styles = StyleSheet.create({
         width: 75,
         height: 75,
         borderRadius: 1200,
+    },
+    item: {
+
     }
 });
 
